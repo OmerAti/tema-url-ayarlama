ab6d90c1c55af5764535f738bcd7caf56230ab24
Ömer ATABER <67821076+OmerAti@users.noreply.github.com>
2024-06-01 22:05:18 +0300

    Update tema-url.php

--- tema-url.php
+++ tema-url.php
@@ -32,115 +32,121 @@ if (!class_exists('CustomThemeSwitcher')) {
             );
         }
 
-        public function ayar_kaydet()
-        {
-            register_setting('tema_degistirici_ayarlar', 'masaustu_tema');
-            register_setting('tema_degistirici_ayarlar', 'amp_tema_url');
-            register_setting('tema_degistirici_ayarlar', 'cache_aktif');
-            register_setting('tema_degistirici_ayarlar', 'yonlendirme_hizli');
-            register_setting('tema_degistirici_ayarlar', 'amp_sayfa_masaustune_gizle');
-        }
+       public function ayar_kaydet()
+{
+    register_setting('tema_degistirici_ayarlar', 'masaustu_tema');
+    register_setting('tema_degistirici_ayarlar', 'amp_tema_url');
+    register_setting('tema_degistirici_ayarlar', 'cache_aktif');
+    register_setting('tema_degistirici_ayarlar', 'yonlendirme_hizli');
+    register_setting('tema_degistirici_ayarlar', 'amp_sayfa_masaustune_gizle');
+    register_setting('tema_degistirici_ayarlar', 'iframe_aktif'); // Yeni ayar
+}
 
-        public function sayfa_icerigi()
-        {
-            ?>
-<div class="wrap">
-    <?php if (isset($_GET['settings-updated']) && $_GET['settings-updated']) : ?>
-        <div id="message" class="updated notice is-dismissible">
-            <p>Ayarlar başarıyla kaydedildi!</p>
-            <button type="button" class="notice-dismiss"><span class="screen-reader-text">Bu mesajı kapat</span></button>
+public function sayfa_icerigi()
+{
+    ?>
+    <div class="wrap">
+        <?php if (isset($_GET['settings-updated']) && $_GET['settings-updated']) : ?>
+            <div id="message" class="updated notice is-dismissible">
+                <p>Ayarlar başarıyla kaydedildi!</p>
+                <button type="button" class="notice-dismiss"><span class="screen-reader-text">Bu mesajı kapat</span></button>
+            </div>
+        <?php endif; ?>
+        <div style="float:left; width: 70%;">
+            <h1><img src="<?php echo plugin_dir_url(__FILE__) . 'logo.png'; ?>" alt="Logo" style="height: 40px; vertical-align: middle;"> Tema URL Ayarları</h1>
+            <form method="post" action="options.php">
+                <?php
+                settings_fields('tema_degistirici_ayarlar');
+                do_settings_sections('tema_degistirici_ayarlar');
+                ?>
+                <table class="form-table">
+                    <tr valign="top">
+                        <th scope="row">Masaüstü Teması</th>
+                        <td>
+                            <select name="masaustu_tema">
+                                <?php
+                                $temalar = wp_get_themes();
+                                $secili_masaustu_tema = get_option('masaustu_tema');
+                                foreach ($temalar as $tema_slug => $tema) {
+                                    echo '<option value="' . esc_attr($tema_slug) . '" ' . selected($secili_masaustu_tema, $tema_slug, false) . '>' . esc_html($tema->get('Name')) . '</option>';
+                                }
+                                ?>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr valign="top">
+                        <th scope="row">AMP veya Mobil Tema URL</th>
+                        <td>
+                            <input type="text" name="amp_tema_url" value="<?php echo esc_attr(get_option('amp_tema_url')); ?>" />
+                            <p class="description">AMP veya Mobil temasının URL'sini girin. Örneğin: https://example.com/amp</p>
+                        </td>
+                    </tr>
+                    <tr valign="top">
+                        <th scope="row">Cache</th>
+                        <td>
+                            <input type="checkbox" name="cache_aktif" <?php checked(get_option('cache_aktif'), 'on'); ?> />
+                            <label>Aktif</label>
+                        </td>
+                    </tr>
+                    <tr valign="top">
+                        <th scope="row">Hızlı Yönlendirme</th>
+                        <td>
+                            <input type="checkbox" name="yonlendirme_hizli" <?php checked(get_option('yonlendirme_hizli'), 'on'); ?> />
+                            <label>Aktif</label>
+                        </td>
+                    </tr>
+                    <tr valign="top">
+                        <th scope="row">Masaüstünde Varsayılan Tema</th>
+                        <td>
+                            <input type="checkbox" name="amp_sayfa_masaustune_gizle" <?php checked(get_option('amp_sayfa_masaustune_gizle'), 'on'); ?> />
+                            <label>Aktif</label>
+                        </td>
+                    </tr>
+                    <tr valign="top">
+                        <th scope="row">URL'yi iframe'de Göster</th>
+                        <td>
+                            <input type="checkbox" name="iframe_aktif" <?php checked(get_option('iframe_aktif'), 'on'); ?> />
+                            <label>Aktif</label>
+                        </td>
+                    </tr>
+                </table>
+                <?php submit_button('Ayarları Kaydet'); ?>
+            </form>
         </div>
-    <?php endif; ?>
-    <div style="float:left; width: 70%;">
-        <h1><img src="<?php echo plugin_dir_url(__FILE__) . 'logo.png'; ?>" alt="Logo" style="height: 40px; vertical-align: middle;"> Tema URL Ayarları</h1>
-        <form method="post" action="options.php">
-            <?php
-            settings_fields('tema_degistirici_ayarlar');
-            do_settings_sections('tema_degistirici_ayarlar');
-            ?>
-            <table class="form-table">
-                <tr valign="top">
-                    <th scope="row">Masaüstü Teması</th>
-                    <td>
-                        <select name="masaustu_tema">
-                            <?php
-                            $temalar = wp_get_themes();
-                            $secili_masaustu_tema = get_option('masaustu_tema');
-                            foreach ($temalar as $tema_slug => $tema) {
-                                echo '<option value="' . esc_attr($tema_slug) . '" ' . selected($secili_masaustu_tema, $tema_slug, false) . '>' . esc_html($tema->get('Name')) . '</option>';
-                            }
-                            ?>
-                        </select>
-                    </td>
-                </tr>
-                <tr valign="top">
-                    <th scope="row">AMP veya Mobil Tema URL</th>
-                    <td>
-                        <input type="text" name="amp_tema_url" value="<?php echo esc_attr(get_option('amp_tema_url')); ?>" />
-                        <p class="description">AMP veya Mobil temasının URL'sini girin. Örneğin: https://example.com/amp</p>
-                    </td>
-                </tr>
-                <tr valign="top">
-                    <th scope="row">Cache</th>
-                    <td>
-                        <input type="checkbox" name="cache_aktif" <?php checked(get_option('cache_aktif'), 'on'); ?> />
-                        <label>Aktif</label>
-                    </td>
-                </tr>
-                <tr valign="top">
-                    <th scope="row">Hızlı Yönlendirme</th>
-                    <td>
-                        <input type="checkbox" name="yonlendirme_hizli" <?php checked(get_option('yonlendirme_hizli'), 'on'); ?> />
-                        <label>Aktif</label>
-                    </td>
-                </tr>
-                <tr valign="top">
-                    <th scope="row">Masaüstünde Varsayılan Tema</th>
-                    <td>
-                        <input type="checkbox" name="amp_sayfa_masaustune_gizle" <?php checked(get_option('amp_sayfa_masaustune_gizle'), 'on'); ?> />
-                        <label>Aktif</label>
-                    </td>
-                </tr>
-            </table>
-            <?php submit_button('Ayarları Kaydet'); ?>
-        </form>
-    </div>
-    <div style="float:right; width: 30%;">
-        <div class="bilgiler">
-            <h2>Bilgilendirme</h2>
-            <ul>
-                <li>Wordpress için Gerekli Modüller:</li>
+        <div style="float:right; width: 30%;">
+            <div class="bilgiler">
+                <h2>Bilgilendirme</h2>
                 <ul>
-        <li>Web Sunucusu: <?php echo server_info_check(); ?></li>
-        <li>Disk Alanı: <?php echo disk_space_check(); ?></li>
-        <li>PHP Sürümü: <?php echo php_version_check(); ?></li>
-        <li>Veritabanı: <?php echo database_check(); ?></li>
-        <li>RAM: <?php echo ram_check(); ?></li>
-        <li>HTTPS Desteği: <?php echo https_support_check(); ?></li>                </ul>
-            </ul>
+                    <li>Wordpress için Gerekli Modüller:</li>
+                    <ul>
+                        <li>Web Sunucusu: <?php echo server_info_check(); ?></li>
+                        <li>Disk Alanı: <?php echo disk_space_check(); ?></li>
+                        <li>PHP Sürümü: <?php echo php_version_check(); ?></li>
+                        <li>Veritabanı: <?php echo database_check(); ?></li>
+                        <li>RAM: <?php echo ram_check(); ?></li>
+                        <li>HTTPS Desteği: <?php echo https_support_check(); ?></li>
+                    </ul>
+                </ul>
+            </div>
         </div>
+        <div style="clear:both;"></div>
+        <div class="footer-text">JRodix.Com Internet Hizmetleri Tarafından Kodlanmış ve Yayınlanmıştır</div>
     </div>
-    <div style="clear:both;"></div>
-    <div class="footer-text">JRodix.Com Internet Hizmetleri Tarafından Kodlanmış ve Yayınlanmıştır</div>
-</div>
-
-
-
-<aside class="amp-bilgilendirme">
-    <h2>Nasıl AMP Sayfası Oluşturulur?</h2>
-    <p>AMP (Accelerated Mobile Pages), web sitelerinin mobil cihazlarda hızlı bir şekilde yüklenmesini sağlayan açık kaynak kodlu bir projesidir. AMP sayfaları, hızlı bir kullanıcı deneyimi sağlamak için optimize edilmiştir.</p>
-    <p>Aşağıdaki adımları izleyerek kendi AMP sayfanızı oluşturabilirsiniz:</p>
-    <ol>
-        <li>HTML belgenizin başlangıcına <code>&lt;!DOCTYPE html&gt;</code> ekleyin.</li>
-        <li><code>&lt;html&gt;</code> etiketine <code>amp</code> özelliği ekleyin: <code>&lt;html ⚡ lang="tr"&gt;</code>.</li>
-        <li><code>&lt;head&gt;</code> bölümüne AMP için gerekli meta etiketlerini ekleyin: viewport, charset, canonical, vb.</li>
-        <li><code>&lt;style amp-custom&gt;</code> etiketi içine özel CSS kodlarınızı ekleyin.</li>
-        <li>Sayfanızın içeriğini oluşturun ve AMP uyumlu bileşenler kullanın.</li>
-        <li>Gerektiğinde AMP JS kitaplığını yükleyin: <code>&lt;script async src="https://cdn.ampproject.org/v0.js"&gt;&lt;/script&gt;</code>.</li>
-        <li>Örnek bir AMP sayfası oluşturun:</li>
-    </ol>
-    <div class="amp-html-example">
-        <textarea id="ampHtmlCode" readonly>&lt;!DOCTYPE html&gt;
+    <aside class="amp-bilgilendirme">
+        <h2>Nasıl AMP Sayfası Oluşturulur?</h2>
+        <p>AMP (Accelerated Mobile Pages), web sitelerinin mobil cihazlarda hızlı bir şekilde yüklenmesini sağlayan açık kaynak kodlu bir projesidir. AMP sayfaları, hızlı bir kullanıcı deneyimi sağlamak için optimize edilmiştir.</p>
+        <p>Aşağıdaki adımları izleyerek kendi AMP sayfanızı oluşturabilirsiniz:</p>
+        <ol>
+            <li>HTML belgenizin başlangıcına <code>&lt;!DOCTYPE html&gt;</code> ekleyin.</li>
+            <li><code>&lt;html&gt;</code> etiketine <code>amp</code> özelliği ekleyin: <code>&lt;html ⚡ lang="tr"&gt;</code>.</li>
+            <li><code>&lt;head&gt;</code> bölümüne AMP için gerekli meta etiketlerini ekleyin: viewport, charset, canonical, vb.</li>
+            <li><code>&lt;style amp-custom&gt;</code> etiketi içine özel CSS kodlarınızı ekleyin.</li>
+            <li>Sayfanızın içeriğini oluşturun ve AMP uyumlu bileşenler kullanın.</li>
+            <li>Gerektiğinde AMP JS kitaplığını yükleyin: <code>&lt;script async src="https://cdn.ampproject.org/v0.js"&gt;&lt;/script&gt;</code>.</li>
+            <li>Örnek bir AMP sayfası oluşturun:</li>
+        </ol>
+        <div class="amp-html-example">
+            <textarea id="ampHtmlCode" readonly>&lt;!DOCTYPE html&gt;
 &lt;html ⚡ lang=&quot;tr&quot;&gt;
 &lt;head&gt;
     &lt;meta charset=&quot;utf-8&quot;&gt;
@@ -156,27 +162,22 @@ if (!class_exists('CustomThemeSwitcher')) {
     &lt;p&gt;Örnek AMP Sayfası&lt;/p&gt;
 &lt;/body&gt;
 &lt;/html&gt;</textarea>
-        <button onclick="copyAmpHtmlCode()">Kopyala</button>
-    </div>
-    <p id="copyStatusMessage"></p>
-</aside>
-
-<style>
-
-	
-</style>
-
-<script>
-    function copyAmpHtmlCode() {
-        var ampHtmlCode = document.getElementById("ampHtmlCode");
-        ampHtmlCode.select();
-        document.execCommand("copy");
-        var copyStatusMessage = document.getElementById("copyStatusMessage");
-        copyStatusMessage.textContent = "AMP HTML kodu kopyalandı!";
-    }
-</script>
-<?php
+            <button onclick="copyAmpHtmlCode()">Kopyala</button>
+        </div>
+        <p id="copyStatusMessage"></p>
+    </aside>
+    <script>
+        function copyAmpHtmlCode() {
+            var ampHtmlCode = document.getElementById("ampHtmlCode");
+            ampHtmlCode.select();
+            document.execCommand("copy");
+            var copyStatusMessage = document.getElementById("copyStatusMessage");
+            copyStatusMessage.textContent = "AMP HTML kodu kopyalandı!";
+        }
+    </script>
+    <?php
 }
+
 
 public function tema_degistir()
 {
@@ -185,13 +186,22 @@ public function tema_degistir()
     $masaustu_tema = get_option('masaustu_tema');
     $amp_tema_url = get_option('amp_tema_url');
     $amp_sayfa_masaustune_gizle = get_option('amp_sayfa_masaustune_gizle');
+    $iframe_aktif = get_option('iframe_aktif');
 
     if (wp_is_mobile() && $amp_tema_url && get_option('yonlendirme_hizli') && !isset($_GET['no_redirect'])) {
         if (!$amp_sayfa_masaustune_gizle || ($amp_sayfa_masaustune_gizle && $amp_tema_url)) {
-            wp_redirect($amp_tema_url);
-            exit;
+            if ($iframe_aktif) {
+                echo '<!DOCTYPE html><html><head><title>Redirecting...</title></head><body>';
+                echo '<iframe src="' . esc_url($amp_tema_url) . '" style="border:none;width:100%;height:100vh;"></iframe>';
+                echo '</body></html>';
+                exit;
+            } else {
+                wp_redirect($amp_tema_url);
+                exit;
+            }
         }
     }
+
     if (!empty($masaustu_tema) && wp_get_theme($masaustu_tema)->exists() && !is_child_theme($masaustu_tema) && !get_option('amp_sayfa_masaustune_gizle')) {
         $current_theme = wp_get_theme();
         $active_theme = $current_theme->get_stylesheet();
@@ -201,6 +211,7 @@ public function tema_degistir()
         }
     }
 }
+
 
 public function enqueue_admin_styles()
 {
